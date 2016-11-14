beae7acfed0794dad15a92415f2ffa6ad3fc66dc
Phuc <hoangphucd3@gmail.com>
2016-11-14 23:45:19 +0700

    update service

---
+++ src/AppBundle/Admin/TourRequestAdmin.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class TourRequestAdmin extends AbstractAdmin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('reason')
+            ->add('departure')
+            ->add('adults')
+//            ->add('children')
+            ->add('infants')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->add('reason')
+            ->add('departure')
+            ->add('adults')
+//            ->add('children')
+            ->add('infants')
+            ->add('_action', null, array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('id')
+            ->add('reason')
+            ->add('departure')
+            ->add('adults')
+//            ->add('children')
+            ->add('infants')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('reason')
+            ->add('departure')
+            ->add('adults')
+//            ->add('children')
+            ->add('infants')
+        ;
+    }
+}
